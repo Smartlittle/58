71d1b132962f63ea014c094eedee504036082576
甬哥侃侃侃ygkkk <renweikg@gmail.com>
2023-01-08 03:39:56 +0000

    删除entrypoint.sh

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Create By ifeng
-# Web Site:https://www.hicairo.com
-# Telegram:https://t.me/HiaiFeng
-
-sed -i "s/uuid/$uuid/g" /etc/xray/config.json
-sed -i "s/uuid/$uuid/g" /etc/nginx/nginx.conf
-exec "$@"
